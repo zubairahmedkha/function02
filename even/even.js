564757e7b98f583680189a83309af33b2f1f6e16
zubairkhan <zubairahmedniz@gmail.com>
2024-03-08 09:18:10 +0500

    work

--- even/even.js
+++ even/even.js
@@ -1,12 +1,12 @@
 "use strict";
-let num1 = 0;
+let num1 = 2;
 if (num1 < 0 && num1 % 2 == 0) {
-    console.log(num1, "this is a Eeven");
+    console.log(num1, "this is a Eeven number");
 }
 else
     (num1 <= 0);
 {
-    console.log(num1, "this is a Eeven");
+    console.log(num1, "this is a Eeven number");
 }
 if (num1 % 2 >= 1) {
     console.log(num1, ",this is a odd");
